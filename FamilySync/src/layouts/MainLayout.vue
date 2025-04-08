b44b54a9bc96662ac649b7c2256cd09fc09dc839
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2025-04-08 23:04:16 +0200

    Feat: display navigation menu as icons

--- FamilySync/src/layouts/MainLayout.vue
+++ FamilySync/src/layouts/MainLayout.vue
@@ -2,13 +2,14 @@
   <q-layout view="lHh Lpr lFf">
     <div class="q-pa-md">
       <q-toolbar class="bg-secondary text-white q-my-md shadow-2">
-        HEADER
+        FamilySync
 
         <q-space />
 
         <q-btn-toggle
           v-model="model"
-          flat stretch
+          flat
+          stretch
           toggle-color="yellow"
           :options="options"
           @update:model-value="onOptionChange"
@@ -29,13 +30,18 @@ import { PATHS } from 'src/router/routes'
 const router = useRouter()
 const model = ref('home') // Default to 'home'
 
-const options = [
-  { label: 'Home', value: PATHS.HOME },
-  { label: 'Calendar', value: PATHS.CALENDAR },
-  { label: 'Shopping list', value: PATHS.SHOPPING_LIST },
-  { label: 'Todo list', value: PATHS.TODO_LIST },
-  { label: 'Settings', value: PATHS.SETTINGS },
-]
+interface OptionType {
+  value: string
+  icon: string
+}
+
+const options: OptionType[] = [
+  { value: PATHS.HOME, icon: 'home' },
+  { value: PATHS.CALENDAR, icon: 'event' },
+  { value: PATHS.SHOPPING_LIST, icon: 'shopping_cart' },
+  { value: PATHS.TODO_LIST, icon: 'checklist' },
+  { value: PATHS.SETTINGS, icon: 'settings' },
+] as const
 
 // Function to handle route changes
 const onOptionChange = (value: string) => {
