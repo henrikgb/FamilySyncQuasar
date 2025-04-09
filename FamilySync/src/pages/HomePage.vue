a386b57054f13a98adb00ea411dbd2c76366cb98
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2025-04-09 10:09:59 +0200

    Feat: update layout in home page

--- FamilySync/src/pages/HomePage.vue
+++ FamilySync/src/pages/HomePage.vue
@@ -1,8 +1,60 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    This is the HomePage
+  <q-page class="column items-center q-pt-xl q-px-md">
+
+    <div class="text-center q-mb-lg">
+      <p class="text-h6 q-mb-xs">Welcome to the FamilySync app.</p>
+      <p class="text-subtitle2">The place where you can plan and schedule daily tasks with your loved ones.</p>
+    </div>
+
+    <div class="column q-gutter-md" style="width: 100%; max-width: 400px;">
+      <q-btn outline class="q-pa-md" style="justify-start">
+        <template #default>
+          <div class="row items-center no-wrap full-width">
+            <q-icon name="event" />
+            <div class="text-left ellipsis q-ml-sm">
+              Schedule your appointments
+            </div>
+          </div>
+        </template>
+      </q-btn>
+
+      <q-btn outline class="q-pa-md" style="justify-start">
+        <template #default>
+          <div class="row items-center no-wrap full-width">
+            <q-icon name="shopping_cart" />
+            <div class="text-left ellipsis q-ml-sm">
+              Plan your grocery shopping
+            </div>
+          </div>
+        </template>
+      </q-btn>
+
+      <q-btn outline class="q-pa-md" style="justify-start">
+        <template #default>
+          <div class="row items-center no-wrap full-width">
+            <q-icon name="checklist" />
+            <div class="text-left ellipsis q-ml-sm">
+              Plan your to-do-lists
+            </div>
+          </div>
+        </template>
+      </q-btn>
+
+      <q-btn outline class="q-pa-md" style="justify-start">
+        <template #default>
+          <div class="row items-center no-wrap full-width">
+            <q-icon name="tune" />
+            <div class="text-left ellipsis q-ml-sm">
+              Adjust app settings
+            </div>
+          </div>
+        </template>
+      </q-btn>
+    </div>
+
   </q-page>
 </template>
+
 
 <script setup lang="ts">
 //...
