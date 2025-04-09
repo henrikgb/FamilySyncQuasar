759fb039edd7e7b975e8ed412bdb7b2ca3d00fb5
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2025-04-09 14:35:22 +0200

    Feat: update layout in CalendarPage.vue

--- FamilySync/src/pages/CalendarPage.vue
+++ FamilySync/src/pages/CalendarPage.vue
@@ -1,9 +1,34 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    This is the CalendarPage
+  <q-page class="column items-center">
+    <q-layout>
+      <div class="row justify-center item-center q-gutter-x-sm q-gutter-y-sm q-mb-sm q-mt-sm">
+        <q-btn @click="setEditCalendarView" class="glossy" color="teal" icon="edit_calendar" label="Edit calendar" />
+        <q-btn @click="setViewCalendarView" class="glossy" color="primary" icon-right="event_available" label="View calendar" />
+      </div>
+
+      <q-layout v-if="isEditCalendar" class="bg-green-3 q-px-lg q-py-lg row justify-center item-center">
+          Edit calendar
+      </q-layout>
+
+      <q-layout v-if="isViewCalendar" class="bg-blue-3 q-px-lg q-py-lg row justify-center item-center">
+          View calendar
+      </q-layout>
+    </q-layout>
   </q-page>
 </template>
 
 <script setup lang="ts">
-//...
+import { ref } from 'vue'
+
+const isEditCalendar = ref(true)
+const isViewCalendar = ref(false)
+
+const setEditCalendarView = () => {
+  isEditCalendar.value = true
+  isViewCalendar.value = false
+}
+const setViewCalendarView = () => {
+  isEditCalendar.value = false
+  isViewCalendar.value = true
+}
 </script>
