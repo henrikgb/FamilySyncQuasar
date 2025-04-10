ee1654bf04e970a346c9c9fe7f9fda5417b39b2c
Henrik Grimsmo Busengdal <henrik-gb@hotmail.com>
2025-04-10 12:42:44 +0200

    Feat: update layout for edit and view calendar content

--- FamilySync/src/pages/calendar/EditCalendar.vue
+++ FamilySync/src/pages/calendar/EditCalendar.vue
@@ -1,9 +1,25 @@
 <template>
-  <q-layout class="bg-green-3 q-px-lg q-py-lg row justify-center item-center">
-      Edit calendar
+  <q-layout class="q-px-lg q-py-lg row justify-center item-center">
+    <div class="q-pa-md" style="max-width: 300px">
+      <q-input filled v-model="date" mask="date" :rules="['date']">
+        <template v-slot:append>
+          <q-icon name="event" class="cursor-pointer">
+            <q-popup-proxy cover transition-show="scale" transition-hide="scale">
+              <q-date v-model="date" color="teal">
+                <div class="row items-center justify-end">
+                  <q-btn v-close-popup label="Close" color="primary" flat />
+                </div>
+              </q-date>
+            </q-popup-proxy>
+          </q-icon>
+        </template>
+      </q-input>
+    </div>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-//..
+import { ref } from 'vue'
+const defaultDate = new Date().toISOString().split('T')?.[0]?.replace(/-/g, '/') ?? ''
+const date = ref(defaultDate)
 </script>
--- FamilySync/src/pages/calendar/ViewCalendar.vue
+++ FamilySync/src/pages/calendar/ViewCalendar.vue
@@ -1,9 +1,49 @@
 <template>
-  <q-layout class="bg-blue-3 q-px-lg q-py-lg row justify-center item-center">
-    View calendar
+  <q-layout class="q-px-lg q-py-lg">
+    <div class="col item-center">
+      <div class="row justify-center q-mb-lg">
+        <q-date
+          v-model="date"
+          :events="events"
+          event-color="orange"
+          color="teal"
+        />
+      </div>
+      <div class="row justify-center">
+        <q-tab-panels
+          v-model="date"
+          animated
+          transition-prev="jump-up"
+          transition-next="jump-up"
+          class="bg-yellow-3 rounded-borders"
+        >
+          <q-tab-panel name="2019/02/01">
+            <div class="text-h4 q-mb-md">2019/02/01</div>
+            <p>TEST 2 Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="2019/02/01">
+            <div class="text-h4 q-mb-md">2019/02/01</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="2019/02/05">
+            <div class="text-h4 q-mb-md">2019/02/05</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+
+          <q-tab-panel name="2019/02/06">
+            <div class="text-h4 q-mb-md">2019/02/06</div>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quis praesentium cumque magnam odio iure quidem, quod illum numquam possimus obcaecati commodi minima assumenda consectetur culpa fuga nulla ullam. In, libero.</p>
+          </q-tab-panel>
+        </q-tab-panels>
+      </div>
+    </div>
   </q-layout>
 </template>
 
 <script setup lang="ts">
-//..
+import { ref } from 'vue'
+const date = ref('2019/02/01')
+const events =  [ '2019/02/01', '2019/02/05', '2019/02/06' ]
 </script>
