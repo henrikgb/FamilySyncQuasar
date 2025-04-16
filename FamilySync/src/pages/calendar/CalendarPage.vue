c4ab529b5c6216ece3ca12bc83d461c9f6569e19
Henrik Grimsmo Busengdal <henrik-gb@gotmail.com>
2025-04-16 10:43:17 +0200

    Feat: update view in CalendarPage.vue

--- FamilySync/src/pages/calendar/CalendarPage.vue
+++ FamilySync/src/pages/calendar/CalendarPage.vue
@@ -2,8 +2,22 @@
   <q-page class="column items-center">
     <q-layout>
       <div class="row justify-center item-center q-gutter-x-sm q-gutter-y-sm q-mb-sm q-mt-sm">
-        <q-btn @click="setEditCalendarView" class="glossy" color="teal" icon="edit_calendar" label="Edit calendar" />
-        <q-btn @click="setViewCalendarView" class="glossy" color="primary" icon-right="event_available" label="View calendar" />
+        <q-btn
+          @click="setViewCalendarView"
+          class="glossy"
+          :color="isViewCalendar ? 'white' : 'primary'"
+          :text-color="isViewCalendar ? 'primary' : 'white'"
+          icon-right="event_available"
+          label="View calendar"
+        />
+        <q-btn
+          @click="setEditCalendarView"
+          class="glossy"
+          :color="isEditCalendar ? 'white' : 'teal'"
+          :text-color="isEditCalendar ? 'teal' : 'white'"
+          icon="edit_calendar"
+          label="Edit calendar"
+        />
       </div>
 
       <EditCalendar v-if="isEditCalendar" />
@@ -19,8 +33,8 @@ import { ref } from 'vue'
 import EditCalendar from './EditCalendar.vue';
 import ViewCalendar from './ViewCalendar.vue';
 
-const isEditCalendar = ref(true)
-const isViewCalendar = ref(false)
+const isViewCalendar = ref(true)
+const isEditCalendar = ref(false)
 
 const setEditCalendarView = () => {
   isEditCalendar.value = true
