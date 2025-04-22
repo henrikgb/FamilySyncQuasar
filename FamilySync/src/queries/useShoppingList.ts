1ec2aaf5f21a958bf17bd200bb2fa26cdbcebca7
Henrik Grimsmo Busengdal <henrik-gb@gotmail.com>
2025-04-23 00:13:07 +0200

    Feat: tell all components using useUpdateShoppingList() to refetch the data

--- FamilySync/src/queries/useShoppingList.ts
+++ FamilySync/src/queries/useShoppingList.ts
@@ -1,8 +1,7 @@
-import { useQuery, useMutation } from '@tanstack/vue-query'
+import { useQuery, useMutation, useQueryClient } from '@tanstack/vue-query'
 import axios from 'axios'
 import { API_BASE } from 'src/constants/api'
 import type { ShoppingListDTO } from 'src/dto/ShoppingListDTO'
-
 
 export function useShoppingList() {
   return useQuery({
@@ -15,9 +14,15 @@ export function useShoppingList() {
 }
 
 export function useUpdateShoppingList() {
+  const queryClient = useQueryClient()
+
   return useMutation({
     mutationFn: async (updatedShoppingList: ShoppingListDTO) => {
       await axios.post(`${API_BASE}/ShoppingList`, updatedShoppingList)
+    },
+    onSuccess: () => {
+      // This tells all components using useUpdateShoppingList() to refetch the data
+      void queryClient.invalidateQueries({ queryKey: ['shoppingList'] })
     },
   })
 }
