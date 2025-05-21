2a1353d345604afb527ff605e28eb9511d74a909
henrikgb <henrik-gb@hotmail.com>
2025-05-21 16:40:02 +0200

    Delete comment

--- FamilySync/src/router/routes.ts
+++ FamilySync/src/router/routes.ts
@@ -52,8 +52,6 @@ const routes: RouteRecordRaw[] = [
     component: () => import('layouts/MainLayout.vue'),
     children: [{ path: '', component: () => import('pages/SettingsPage.vue') }],
   },
-  // Always leave this as last one,
-  // but you can also remove it
   {
     path: '/:catchAll(.*)*',
     component: () => import('pages/ErrorNotFound.vue'),
