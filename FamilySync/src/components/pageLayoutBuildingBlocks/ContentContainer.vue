9f32aec85d5e2299f224464c2b5a5ffcc4e982df
henrikgb <henrik-gb@hotmail.com>
2025-05-22 14:12:21 +0200

    Update ContentContainer

--- FamilySync/src/components/pageLayoutBuildingBlocks/ContentContainer.vue
+++ FamilySync/src/components/pageLayoutBuildingBlocks/ContentContainer.vue
@@ -1,5 +1,15 @@
 <template>
-  <q-card class="bg-yellow-3 q-pa-md rounded-borders">
+  <q-card :class="['q-pa-md', 'rounded-borders', backgroundClass]">
     <slot />
   </q-card>
 </template>
+
+<script setup lang="ts">
+import { defineProps, computed } from 'vue';
+
+const props = defineProps<{
+  definedBackgroundColor?: string;
+}>();
+
+const backgroundClass = computed(() => props.definedBackgroundColor ?? 'bg-yellow-3');
+</script>
