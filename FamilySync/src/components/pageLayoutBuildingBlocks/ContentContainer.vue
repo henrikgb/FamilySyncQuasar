1f2ede3974632fc73ff696a45c32e5e677ae3624
henrikgb <henrik-gb@hotmail.com>
2025-05-22 11:30:09 +0200

    Feat: add layout building blocks

---
+++ FamilySync/src/components/pageLayoutBuildingBlocks/ContentContainer.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="w-full bg-yellow-3 q-mt-md q-px-md q-pa-md rounded-borders">
+    <slot />
+  </div>
+</template>
---
+++ FamilySync/src/components/pageLayoutBuildingBlocks/LoadingAnimation.vue
@@ -0,0 +1,6 @@
+<template>
+  <div class="column items-center justify-center q-gutter-sm">
+    <q-spinner color="teal" size="50px" />
+    <text-h4>Loading</text-h4>
+  </div>
+</template>
