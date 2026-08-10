f0b487d77140daa585933450765fb42e40e34178
henrikgb <henrik-gb@hotmail.com>
2026-08-10 21:02:03 +0200

    Style: Adjust font size for loading animation

--- FamilySync/src/components/pageLayoutBuildingBlocks/LoadingAnimation.vue
+++ FamilySync/src/components/pageLayoutBuildingBlocks/LoadingAnimation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="column items-center justify-center q-gutter-sm">
     <q-spinner color="teal" size="50px" />
-    <div class="text-h4">Loading</div>
+    <div class="text-h6">Loading</div>
   </div>
 </template>
